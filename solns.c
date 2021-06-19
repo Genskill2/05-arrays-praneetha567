267cdeaa1217d9e65b7b810d21db5c16dd4aff81
Praneetha <praneethadinesh@gmail.com>
2021-06-19 14:51:53 +0530

    Solutions

--- solns.c
+++ solns.c
@@ -51,46 +51,48 @@ int mode(int d[],int n4)
 
 int isprime(int);
 
-int isprime(int i)
-{ int flag=0;
-  for(int j=2;j<1;++j)
-   { if(i%j==0)
-     {flag=1;
-      break;
-     }
-   }  
-   if(flag==0)
-     return 1;
-   else
-     return 0;
-}
-
-       
+ int isprime(int i)
+    {
+	int flag=0;
+			for(int j=2;j<i;j++)
+			{
+				if(i%j==0)
+				{
+					flag=1;
+					break;
+				}
+			}
+			if(flag==0)
+			    return 1;
+			else
+			    return 0;
+    }
 int factors(int n,int a[])
- {int count=0;
-   int i=2;
-   while(i<n)
-   {
-     if(n%i==0)
-     if(isprime(i)==1)
-     { a[count]=i;
-       count++;
-          n=n/i;
-          if(isprime(n)==1)
-          {
-           a[count++]=n;
-           break;
-          }
-          i=2;
-     }
-     else continue;
-     else;
-     i++;
-   }
-   return count;
-  }         
+{
+	int count=0;
+    	int i=2;
+    	while(i<n)
+    	{
+        	if(n%i==0)
+			if(isprime(i)==1)
+			{	a[count]=i;
+				count++;
+                		n=n/i;
+                		if(isprime(n)==1)
+                		{
+                			a[count++]=n;
+                			break;
+                		}
+                		i=2;
+			}
+		else continue;
+		else
+		i++;
+	}
+	return count;
+}
   
- 
+
  
  
  
